3e0ffafb5a7ca2f5b928b4eb28d2f0a4f2a0da5f
Tyuiu.HudojberdievDB@outlook.com <Tyuiu.HudojberdievDB@outlook.com>
2024-11-26 20:35:47 +0300

    Add project files.

---
+++ Tyuiu.KhudoiberdievDB.Sprint5.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KhudoiberdievDB.Sprint5.Task0.V7.Lib;
+
+
+public class DataService : ISprint5Task0V7
+
+{
+    public string SaveToFileTextData(int x)
+    {
+        string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+        double z= -(Math.Pow(x,3))+ 4*(Math.Pow(x,2))- 3/2*x;
+        z = Math.Round(z, 3);
+        File.WriteAllText(path, Convert.ToString(z));
+        return path;
+    }
+}
---
+++ Tyuiu.KhudoiberdievDB.Sprint5.Task0.V7/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KhudoiberdievDB.Sprint5.Task0.V7.Lib;
+namespace Tyuiu.KhudoiberdievDB.Sprint5.Task0.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Худойбердиев Д. Б. | ИСПБ 24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Худойбердиев Далер Бахритдинович | ИСПБ 24-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значение выражения, результат сохранить в текстовый файл и    *");
+            Console.WriteLine("* вывести на консоль.                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}

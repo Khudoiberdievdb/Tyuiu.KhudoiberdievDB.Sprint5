02218ae3e5d935db3affb91f8f5e79386fb3639f
Tyuiu.HudojberdievDB@outlook.com <Tyuiu.HudojberdievDB@outlook.com>
2024-11-26 20:40:08 +0300

    Task0

--- Tyuiu.KhudoiberdievDB.Sprint5.Task0.V7.Lib/DataService.cs
+++ Tyuiu.KhudoiberdievDB.Sprint5.Task0.V7.Lib/DataService.cs
@@ -9,7 +9,7 @@ public class DataService : ISprint5Task0V7
     public string SaveToFileTextData(int x)
     {
         string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
-        double z= -(Math.Pow(x,3))+ 4*(Math.Pow(x,2))- 3/2*x;
+        double z= (-(Math.Pow(x, 3))) + 4 * (Math.Pow(x, 2)) - (3.0 / 2.0 * x);
         z = Math.Round(z, 3);
         File.WriteAllText(path, Convert.ToString(z));
         return path;

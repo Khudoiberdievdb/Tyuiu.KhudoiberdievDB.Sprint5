aae03175f4da74c9655c172a1362c34d48673b28
Tyuiu.HudojberdievDB@outlook.com <Tyuiu.HudojberdievDB@outlook.com>
2024-12-01 20:18:56 +0300

    Task1

--- Tyuiu.KhudoiberdievDB.Sprint5.Task1.V15.Lib/DataService.cs
+++ Tyuiu.KhudoiberdievDB.Sprint5.Task1.V15.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.KhudoiberdievDB.Sprint5.Task1.V15.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
             FileInfo fileInfo = new FileInfo(path);
             bool fileExist = fileInfo.Exists;
 

5744ea2599ed5c33542edf886c05818e39c7b5ec
Tyuiu.HudojberdievDB@outlook.com <Tyuiu.HudojberdievDB@outlook.com>
2024-12-03 17:07:47 +0300

    Task4

--- Tyuiu.KhudoiberdievDB.Sprint5.Task4.V8.Lib/DataService.cs
+++ Tyuiu.KhudoiberdievDB.Sprint5.Task4.V8.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.KhudoiberdievDB.Sprint5.Task4.V8.Lib
         public double LoadFromDataFile(string path)
         {
             string strx = File.ReadAllText(path);
-            double x = Convert.ToDouble(strx);
+            double x = Convert.ToDouble(strx.Replace(".", ","));
             double res = Math.Round((Math.Pow(x,2) / Math.Sin(x)) + 3,3) ;
             return res;
         }
